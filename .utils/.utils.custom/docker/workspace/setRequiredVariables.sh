9472d9f28f842f2535bec469315ce0a7a63ebf5d
Arthur LORENT <arthur@acid.fr>
2019-04-04 18:27:58 +0200

    added submodule

--- .utils/.utils.custom/docker/workspace/setRequiredVariables.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# example custom script
-
-echo -e "${gray}=================================================${reset}\n"
-
-echo "${purple}▶${reset} Setting up script variables ..."
-dockerUser="laradock"
-echo -e "${green}✔${reset} Script variables defined.\n"
--- .utils/.utils.custom/dump/prodToPreprod/generateProductionSqlDump.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# example custom script
-
-dumpProdToPreprodGenerateSqlDumpScriptDirectory=$(dirname "$(readlink -f ${BASH_SOURCE[0]})")
-
-source $(realpath ${dumpProdToPreprodGenerateSqlDumpScriptDirectory}/../../../.utils/database/generateMysqlDump.sh) ${serverProductionSqlDumpStoragePath}/dump.sql
