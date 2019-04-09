98846bf0a24361e364c639d462bfdd5bfc44b27e
Arthur LORENT <arthur@acid.fr>
2019-04-09 11:44:08 +0200

    updated admin translations

--- resources/lang/fr/admin.php
+++ resources/lang/fr/admin.php
@@ -5,12 +5,12 @@ return [
         'orphan' => [
             'index'  => ':entity > Gestion',
             'create' => ':entity > Ajout',
-            'edit'   => ':entity > Édition > :detail',
+            'edit'   => ':entity > Édition > « :detail »',
         ],
         'parent' => [
             'index'  => ':parent > :entity > Gestion',
             'create' => ':parent > :entity > Ajout',
-            'edit'   => ':parent > :entity > Édition > :detail',
+            'edit'   => ':parent > :entity > Édition > « :detail »',
         ],
     ],
     'section' => [
