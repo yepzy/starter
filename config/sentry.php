650d8c2825d21c3778b25cb007fde6049efd1227
acid-cedric <cedric@acid.fr>
2020-02-14 12:30:50 +0100

    Update sentry.php (#21)

--- config/sentry.php
+++ config/sentry.php
@@ -1,7 +1,8 @@
 <?php
 
-return array(
-    'dsn' => env('SENTRY_LARAVEL_DSN'),
+return [
+
+    'dsn'     => env('SENTRY_LARAVEL_DSN', env('SENTRY_DSN')),
 
     // capture release as git sha
     'release' => env('APP_ENV') === 'production'
@@ -9,9 +10,17 @@ return array(
         : trim(exec('git --git-dir ' . base_path('.git') . ' log --pretty="%h" -n1 HEAD')),
 
     'breadcrumbs' => [
+        // Capture Laravel logs in breadcrumbs
+        'logs'         => true,
+
+        // Capture SQL queries in breadcrumbs
+        'sql_queries'  => true,
 
         // Capture bindings on SQL queries logged in breadcrumbs
         'sql_bindings' => true,
 
+        // Capture queue job information in breadcrumbs
+        'queue_info'   => true,
     ],
-);
+
+];
