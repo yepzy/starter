ddbc730f6686d25732cc9f29eb81188b10d14da7
Arthur LORENT <arthur@acid.fr>
2020-03-17 11:51:38 +0100

    fixed air-datetimepicker behavior

--- resources/js/scripts/common/utils/air-datepicker.js
+++ resources/js/scripts/common/utils/air-datepicker.js
@@ -1,23 +1,33 @@
 import 'air-datepicker/src/js/air-datepicker';
 import 'air-datepicker/dist/js/i18n/datepicker.fr';
+const moment = require('moment');
 
 const baseConfig = {
-    language: 'fr',
-    navTitles: {
-        days: 'MM yyyy'
-    },
-    dateFormat: 'dd/mm/yyyy',
+    language: app.locale,
+    navTitles: {days: 'MM yyyy'},
     position: 'top left'
 };
-const dateTimeElements = $('.datetime-picker');
-_.each(dateTimeElements, (dateTimeElement) => {
-    const $this = $(dateTimeElement);
-    const inputMomentDate = moment($this.val(), 'DD/MM/YYYY hh:mm');
-    const datePicker = $this.datepicker({
-        ...baseConfig, ...{
-            timepicker: true,
-            timeFormat: 'hh:ii'
-        }
+
+const selectDateTime = (datePicker, $datePicker) => {
+    const filledDate = moment($datePicker.val(), 'DD/MM/YYYY hh:mm');
+    if (filledDate.isValid()) {
+        datePicker.data('datepicker').selectDate(filledDate.toDate());
+    }
+};
+
+window.triggerDateTimePickerElementsDetection = () => {
+    const dateTimeElements = $('.datetime-picker');
+    _.each(dateTimeElements, (dateTimeElement) => {
+        const $datePicker = $(dateTimeElement);
+        const datePicker = $datePicker.datepicker({
+            ...baseConfig, ...{
+                timepicker: true,
+                dateFormat: 'dd/mm/yyyy',
+                timeFormat: 'hh:ii',
+                onShow: () => selectDateTime(datePicker, $datePicker)
+            }
+        });
     });
-    datePicker.data('datepicker').selectDate(inputMomentDate.toDate());
-});
+};
+
+window.triggerDateTimePickerElementsDetection();
