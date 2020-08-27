90b9f8cbeb405cdc0527d52bcdbf9e5bebb38af7
Arthur LORENT <arthur@acid.fr>
2020-08-27 11:46:12 +0200

    Updated AbstractCommand

--- app/Console/Commands/CommandAbstract.php
+++ app/Console/Commands/CommandAbstract.php
@@ -4,33 +4,14 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
-use InvalidArgumentException;
 
 abstract class CommandAbstract extends Command
 {
-    protected function log(string $message, ?string $level = 'info'): void
+    protected function log(string $message, string $level): void
     {
-        $acceptedLevels = ['title', 'info', 'success', 'error'];
-        if (! (in_array($level, $acceptedLevels))) {
-            throw new InvalidArgumentException('Invalid level provided. Level should be one of these: '
-                . collect($acceptedLevels)->implode(', ') . '. ' . $level . ' given.');
-        }
         if (config('app.debug')) {
             Log::debug($message);
         }
-        switch ($level) {
-            case 'title':
-                $this->output->title($message);
-                break;
-            case 'info':
-                $this->output->comment($message);
-                break;
-            case 'success':
-                $this->output->success($message);
-                break;
-            case 'error':
-                $this->output->error($message);
-                break;
-        }
+        $this->output->{$level}($message);
     }
 }
