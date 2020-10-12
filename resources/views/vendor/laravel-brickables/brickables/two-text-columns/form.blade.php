7c1c4515a9b53fbdbe456ad68105fa690e0ee06d
Arthur LORENT <arthur@acid.fr>
2020-10-12 13:38:08 +0200

    Fixed testing config

--- resources/views/vendor/laravel-brickables/brickables/two-text-columns/form.blade.php
+++ resources/views/vendor/laravel-brickables/brickables/two-text-columns/form.blade.php
@@ -7,6 +7,7 @@
         ->componentHtmlAttributes(['required', 'data-editor']) }}
     {{ textarea()->name('text_right')
         ->locales(supportedLocaleKeys())
-        ->prepend(null)->value(fn($locale) => translatedData($brick, 'data.text_right', $locale))
+        ->prepend(null)
+        ->value(fn($locale) => translatedData($brick, 'data.text_right', $locale))
         ->componentHtmlAttributes(['required', 'data-editor']) }}
 @endsection
