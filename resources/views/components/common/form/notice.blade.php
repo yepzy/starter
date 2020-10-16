744730229e8999b198e2f0a6ca9227d22598ce25
Artu1000 <arthur.lorent@gmail.com>
2020-10-17 01:33:29 +0200

    Updated testing config

--- resources/views/components/common/form/notice.blade.php
+++ resources/views/components/common/form/notice.blade.php
@@ -1,4 +1,4 @@
-<div id="form-notice" class="form-group mb-3">
+<div class="form-group mb-3">
     <span class="text-danger">*</span>
     <span class="small">
         @lang('Required fields.')
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,18 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Storage;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        // We store files in a testing directory.
+        Storage::fake('public');
+        // We config spatie/laravel-medialibrary to store files in testing directory.
+        config()->set('media-library.disk_name', 'public');
+    }
 }
