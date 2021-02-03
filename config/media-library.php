4b5dca4a798d4b9e517713b0353a99911dcef3ab
Arthur LORENT <arthur@acid.fr>
2021-02-03 14:57:17 +0100

    Fixed deploy script with parallel testing

--- config/media-library.php
+++ config/media-library.php
@@ -36,6 +36,18 @@ return [
     //'temporary_upload_model' => Spatie\MediaLibraryPro\Models\TemporaryUpload::class,
 
     /*
+     * When enabled, Media Library Pro will only process temporary uploads there were uploaded
+     * in the same session. You can opt to disable this for stateless usage of
+     * the pro components.
+     */
+    //'enable_temporary_uploads_session_affinity' => true,
+
+    /*
+     * When enabled, Media Library pro will generate thumbnails for uploaded file.
+     */
+    //'generate_thumbnails_for_temporary_uploads' => true,
+
+    /*
      * This is the class that is responsible for naming generated files.
      */
     'file_namer' => Spatie\MediaLibrary\Support\FileNamer\DefaultFileNamer::class,
