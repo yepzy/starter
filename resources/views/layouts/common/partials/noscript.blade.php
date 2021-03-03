28afbce76fa0618124af05e4f77679598f6e2591
Arthur LORENT <arthur@acid.fr>
2021-03-03 11:05:21 +0100

    Fixed noscript handling

--- resources/views/layouts/common/partials/noscript.blade.php
+++ resources/views/layouts/common/partials/noscript.blade.php
@@ -1,13 +1,7 @@
 <noscript class="d-flex justify-content-center bg-danger text-white p-4">
-    @if($gtmId = settings()->google_tag_manager_id)
-        <iframe src="https://www.googletagmanager.com/ns.html?id={{ $gtmId }}"
-                height="0"
-                width="0"
-                style="display:none;visibility:hidden;"></iframe>
-    @endif
-    <div class="noscript">
+    <div>
         <h3>
-            <i class="fa fa-exclamation-triangle"></i>
+            <i class="fa fa-exclamation-triangle fa-fw"></i>
             {{ __('Beware') }}
         </h3>
         {{ __('Javascript is currently disabled in your browser and you are currently browsing in a degraded version. Thank you for reactivating your Javascript to benefit from all the features offered by our web platform.') }}
