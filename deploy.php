a832e3001d0e7fa7c4a99e8e3edd5f1e93acb0a4
Arthur LORENT <arthur@acid.fr>
2021-03-17 13:48:54 +0100

    Removed useless ext-json dependency checks

--- deploy.php
+++ deploy.php
@@ -130,7 +130,6 @@ task('project:dependencies_check', function () {
         'php7.4-fpm',
         'php7.4-common',
         'php7.4-bcmath',
-        'php7.4-json',
         'php7.4-mbstring',
         'php7.4-intl',
         'php7.4-xml',
