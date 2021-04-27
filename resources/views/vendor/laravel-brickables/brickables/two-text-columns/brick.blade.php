3281a30c757e8313f540ee2b84bd674621ecf60c
Arthur LORENT <arthur@acid.fr>
2021-04-27 15:22:52 +0200

    Fixed two text columns brick

--- resources/views/vendor/laravel-brickables/brickables/two-text-columns/brick.blade.php
+++ resources/views/vendor/laravel-brickables/brickables/two-text-columns/brick.blade.php
@@ -3,7 +3,7 @@
         <div class="col-md-6 text">
             {!! (new Parsedown)->text(translatedData($brick, 'data.text_left')) !!}
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6 text">
             {!! (new Parsedown)->text(translatedData($brick, 'data.text_right')) !!}
         </div>
     </div>
