a3d396af99da8c598cdcc45f914c2ac204c92332
Arthur LORENT <arthur@acid.fr>
2021-05-25 09:24:50 +0200

    Fixed free pages seeder

--- database/seeders/PagesSeeder.php
+++ database/seeders/PagesSeeder.php
@@ -10,14 +10,36 @@ class PagesSeeder extends Seeder
     /** @throws \Exception */
     public function run(): void
     {
-        Page::factory()->withTitleBrick()->withOneTextColumnBrick()->withSeoMeta()->create([
-            'unique_key' => 'terms_of_service_page',
-            'nav_title' => ['fr' => 'CGU et mentions légales', 'en' => 'Terms and legal notice'],
-        ]);
-        Page::factory()->withTitleBrick()->withOneTextColumnBrick()->withSeoMeta()->create([
-            'unique_key' => 'gdpr_page',
-            'nav_title' => ['fr' => 'Vie privée et RGPD', 'en' => 'Privacy policy and GDPR'],
-        ]);
-        Page::factory()->withTitleBrick()->withOneTextColumnBrick()->withSeoMeta()->count(3)->create();
+        Page::factory()
+            ->withSpacerBrick('xl')
+            ->withTitleBrick()
+            ->withSpacerBrick('sm')
+            ->withOneTextColumnBrick()
+            ->withSpacerBrick('xl')
+            ->withSeoMeta()
+            ->create([
+                'unique_key' => 'terms_of_service_page',
+                'nav_title' => ['fr' => 'CGU et mentions légales', 'en' => 'Terms and legal notice'],
+            ]);
+        Page::factory()
+            ->withSpacerBrick('xl')
+            ->withTitleBrick()
+            ->withSpacerBrick('sm')
+            ->withOneTextColumnBrick()
+            ->withSpacerBrick('xl')
+            ->withSeoMeta()
+            ->create([
+                'unique_key' => 'gdpr_page',
+                'nav_title' => ['fr' => 'Vie privée et RGPD', 'en' => 'Privacy policy and GDPR'],
+            ]);
+        Page::factory()
+            ->withSpacerBrick('xl')
+            ->withTitleBrick()
+            ->withSpacerBrick('sm')
+            ->withOneTextColumnBrick()
+            ->withSpacerBrick('xl')
+            ->withSeoMeta()
+            ->count(3)
+            ->create();
     }
 }
